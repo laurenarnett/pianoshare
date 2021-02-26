56af10d5dc87d22f562d94441f77be9445770b2a
Lauren Arnett <lba2138@columbia.edu>
2021-02-25 21:52:42 -0500

    add spaces table

--- Application/Schema.sql
+++ Application/Schema.sql
@@ -9,6 +9,32 @@ CREATE TABLE users (
     locked_at TIMESTAMP WITH TIME ZONE DEFAULT NULL,
     failed_login_attempts INT DEFAULT 0 NOT NULL
 );
-CREATE TABLE pianos (
-    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL
+CREATE TABLE spaces (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+    owner_id UUID NOT NULL,
+    summary TEXT NOT NULL,
+    locality TEXT NOT NULL,
+    administrative_area TEXT NOT NULL,
+    country TEXT NOT NULL,
+    postal_code TEXT NOT NULL,
+    thoroughfare TEXT NOT NULL,
+    premise TEXT NOT NULL,
+    price DOUBLE PRECISION NOT NULL,
+    latitude REAL NOT NULL,
+    longitude REAL NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
+    space_type TEXT NOT NULL
 );
+CREATE TABLE reservations (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+    user_id UUID NOT NULL,
+    space_id UUID NOT NULL,
+    start_datetime TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
+    end_datetime TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL
+);
+ALTER TABLE reservations ADD CONSTRAINT reservations_ref_space_id FOREIGN KEY (space_id) REFERENCES spaces (id) ON DELETE NO ACTION;
+ALTER TABLE reservations ADD CONSTRAINT reservations_ref_user_id FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE NO ACTION;
+ALTER TABLE spaces ADD CONSTRAINT spaces_ref_owner_id FOREIGN KEY (owner_id) REFERENCES users (id) ON DELETE NO ACTION;
